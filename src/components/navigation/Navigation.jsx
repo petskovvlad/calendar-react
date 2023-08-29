fdd9e02e583dc770caa8a0c540e585864b57436e
Vladyslav Petskkov <petskovvlad@gmail.com>
2023-08-29 19:40:27 +0300

    highlight of the current day

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { days } from '../../utils/dateUtils.js';
+import { isSameDay } from 'date-fns';
+
+import './navigation.scss'
+
 
 const Navigation = ({ weekDates }) => {
+  const currentDate = new Date();
+
   return (
     <header className="calendar__header">
       {weekDates.map((dayDate) => (
         <div className="calendar__day-label day-label">
           <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-          <span className="day-label__day-number">{dayDate.getDate()}</span>
+          <span className={`day-label__day-number ${isSameDay(dayDate, currentDate) ? 'current-day' : ''}`}>{dayDate.getDate()}</span>
         </div>
       ))}
     </header>
